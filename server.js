cec144c3943ff7118c7c7fa8d9fcbc60a9fb516e
unknown <men.nasies@gmail.com>
2018-08-20 10:44:50 +0300

    customize server.js file

--- server.js
+++ server.js
@@ -1,1 +1,12 @@
-const ex
+const express = require('express');
+const path = require('path');
+const serveStatic = require('serve-static');
+const app= express();
+
+app.use(serveStatic(__dirname + "/dist"));
+
+const port = process.env.PORT || 5000;
+
+app.listen(port);
+
+console.log('server started '+ port);
