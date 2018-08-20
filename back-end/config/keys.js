11485c69c6bea45074e9728e12562ffcfffdf062
unknown <men.nasies@gmail.com>
2018-08-20 11:10:37 +0300

    server.js configurations

--- back-end/config/keys.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    mongoURI: "mongodb://Menios:nas23506@ds115442.mlab.com:15442/my-website",
-    secretOrKey:'W3LC0M3 AG..AIN'
-};
-
--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,6 @@ module.exports = {
     publicPath: '/dist/',
     filename: 'build.js'
   },
-  mode: 'production',
   plugins: [
     new webpack.ProvidePlugin({
       $: 'jquery',
